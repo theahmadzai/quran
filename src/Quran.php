1923f9b833da5fc6902082a3faffb61929d24ed8
Javed Ahmadzai <theahmadzai@hotmail.com>
2017-10-01 19:09:37 +0500

    use method chaning

--- src/Quran.php
+++ src/Quran.php
@@ -2,37 +2,10 @@
 
 namespace Quran;
 
-class Quran extends Chain
+class Quran
 {
-    protected static $factory;
-
     public static function __callStatic(string $name, array $arguments)
     {
-        $chain = new static();
-
-        return $chain->__call($name, $arguments);
-    }
-
-    public function __call(string $name, array $arguments)
-    {
-        return $this->addChain(static::buildChain($name, $arguments));
-    }
-
-    public static function buildChain(string $method, array $arguments = [])
-    {
-        try {
-            return static::getFactory()->chain($method, $arguments);
-        } catch (\Exception $e) {
-            throw new \Exception("ERROR IN CHAIN: " . $e);
-        }
-    }
-
-    public static function getFactory()
-    {
-        if (!static::$factory instanceof Factory) {
-            static::$factory = new Factory();
-        }
-
-        return static::$factory;
+        pr($arguments);
     }
 }

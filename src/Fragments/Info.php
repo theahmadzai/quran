3029209070e3fa46ddded013a99a09a210c8e747
Javed Ahmadzai <theahmadzai@hotmail.com>
2017-10-01 23:48:18 +0500

    Added new methods to class

---
+++ src/Fragments/Info.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Quran\Fragments;
+
+use Quran\Base;
+
+class Info extends Base
+{
+    public function cache()
+    {
+
+    }
+
+    public function fetch($arg)
+    {
+        return $arg;
+    }
+}

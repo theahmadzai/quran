45f4a56726d46774db18255b0a9c68e42f5d9351
Javed Ahmadzai <theahmadzai@hotmail.com>
2017-10-01 23:48:24 +0500

    Added new methods to class

---
+++ src/Fragments/Tafsir.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Quran\Fragments;
+
+use Quran\Base;
+
+class Tafsir extends Base
+{
+    public function cache()
+    {
+
+    }
+
+    public function fetch($arg)
+    {
+        return $arg;
+    }
+}

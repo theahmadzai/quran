0f1d3296f8754f38a7353bc14f4dd1452aff1f17
Javed Ahmadzai <theahmadzai@hotmail.com>
2017-10-05 20:40:15 +0500

    Add Main class adopt new design pattern instead of IDSL

--- src/Quran.php
+++ src/Quran.php
@@ -3,55 +3,29 @@
 namespace Quran;
 
 use Exception;
-use ReflectionClass;
 
-class Quran extends Base
+class Quran
 {
-    protected $fragments;
-
-    public static function settings(array $settings)
-    {
-    }
-
-    public function get(array $arguments = [])
-    {
-        $total = [];
-        foreach ($this->fragments as $fragment) {
-            $total[] = $fragment->build();
-        }
-
-        pr($this->fetchData($total));
-
-        return $this->data;
-    }
-
     public static function __callStatic(string $name, array $arguments)
     {
-        return (new static )->__call($name, $arguments);
+        return (new static())->__call($name, $arguments);
     }
 
     public function __call(string $name, array $arguments)
     {
-        try {
-            $className = 'Quran\\Fragments\\' . ucfirst($name);
+        $classname = '\\Quran\\Rules\\' . ucfirst($name);
 
-            if (!class_exists($className)) {
-                throw new Exception(
-                    sprintf('"%s" is not a valid chain', $name)
-                );
-            }
-
-            $fragment = (new ReflectionClass($className))->newInstanceArgs([$name, $arguments]);
-
-            $this->fragments[spl_object_hash($fragment)] = $fragment;
-        } catch (Exception $exception) {
+        if (!class_exists($classname)) {
             throw new Exception(
-                $exception->getMessage(),
-                $exception->getCode(),
-                $exception
+                sprintf("Class '%s' not found, Pleaes type valid chain.", $classname)
             );
         }
 
         return $this;
+    }
+
+    public function get(array $arguments)
+    {
+        return 'abc';
     }
 }
